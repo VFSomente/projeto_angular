07d4db2414eb5f7d3ba5fac5378b697a7dc4fbb8
louisemdn <glayanelouise22@gmail.com>
2024-11-13 20:04:56 -0300

    projeto angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DetalheComponent } from './page/detalhe/detalhe.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'detalhe',
+    component: DetalheComponent,
+  },
+  { path: '', component: HomeComponent },
+];
